36deca4814b0c7e154135acd98af85332dc2e0e4
qinyoubing <qinyoubing@elihr.cn>
2022-05-31 01:03:39 +0800

    first commit

---
+++ es/ai-video-interview.js
@@ -0,0 +1,2 @@
+import{jsx as r}from"react/jsx-runtime";var e=function(e){return r("div",{children:e.name})};export{e as Test};
+//# sourceMappingURL=eli-react-sdk.js.map
---
+++ src/components/Test/index.tsx
@@ -0,0 +1,13 @@
+import React, { FC } from 'react';
+
+interface TestProps {
+  name: string
+}
+
+const Test: FC<TestProps> = (props: { name: any; }) => {
+  return (
+    <div>{props.name}</div>
+  )
+}
+
+export default Test;
